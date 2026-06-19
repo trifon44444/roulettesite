c9a7de9806647a51df5fec448e58a028cf22b20a
trifon444 <egorpopa367@gmail.com>
2026-06-19 18:20:15 +0300

    script.js

--- script.js
+++ script.js
@@ -1,98 +1,46 @@
-const prizes = [
-{img:"038.png", name:"Деньги"},
-{img:"103.png", name:"Донат"},
-{img:"115.png", name:"Модник"},
-{img:"116.png", name:"Араб"},
+body{
+margin:0;
+background:#0f0f0f;
+color:white;
+font-family:Arial;
+text-align:center;
+}
 
-{img:"14195.png", name:"Мешок"},
-{img:"14196.png", name:"Посох вьюга"},
-{img:"14197.png", name:"Посох снежинка"},
-{img:"14200.png", name:"Галстук"},
-{img:"14202.png", name:"Борода"},
-{img:"14206.png", name:"Ободок звезды"},
-{img:"14207.png", name:"Рюкзак"},
+.main{
+padding:40px;
+}
 
-{img:"14779.png", name:"Крылья бирюза"},
-{img:"14780.png", name:"Крылья красные"},
-{img:"14781.png", name:"Крылья синие"},
-{img:"14797.png", name:"Броник Gucci"},
-{img:"14798.png", name:"Броник LV"},
+#result{
+font-size:40px;
+margin:30px;
+}
 
-{img:"15320.png", name:"Моргенштерн"},
-{img:"15331.png", name:"Янг Харди"},
-{img:"15333.png", name:"Барецкий"},
+button{
+padding:15px 40px;
+font-size:22px;
+border:none;
+border-radius:12px;
+cursor:pointer;
+background:#ffcc00;
+}
 
-{img:"15414.png", name:"Супер сус"},
-{img:"15421.png", name:"Слава Марлоу"},
-{img:"15490.png", name:"Тедди"},
+.strip{
+display:flex;
+gap:15px;
+overflow:hidden;
+justify-content:center;
+margin-top:40px;
+}
 
-{img:"15501.png", name:"Никита Боссов"},
-{img:"15519.png", name:"Майрс"},
+.card{
+background:#1c1c1c;
+padding:15px;
+border-radius:15px;
+min-width:160px;
+}
 
-{img:"15736.png", name:"Ёлка авто"},
-{img:"15738.png", name:"Сноуборд авто"},
-{img:"15739.png", name:"Багажник"},
-{img:"15740.png", name:"Круг авто"},
-
-{img:"15851.png", name:"Сумка Bape"},
-{img:"15888.png", name:"Кобура Gucci"},
-
-{img:"15902.png", name:"Шлем"},
-
-{img:"16224.png", name:"Кепка"},
-
-{img:"16463.png", name:"Скейт"},
-{img:"16464.png", name:"Скейт"},
-{img:"16465.png", name:"Скейт"},
-
-{img:"16600.png", name:"Рука"},
-{img:"16608.png", name:"Рюкзак фиолет"},
-{img:"16665.png", name:"Меч красный"},
-{img:"16678.png", name:"Скейт череп"},
-{img:"16679.png", name:"Скейт фиолет"},
-{img:"16682.png", name:"Меч синий"},
-
-{img:"16774.png", name:"Топор"},
-{img:"16779.png", name:"Плащ"},
-
-{img:"16866.png", name:"Бампер авто"},
-
-{img:"17249.png", name:"Рюкзак Bape"},
-{img:"17250.png", name:"Бита"},
-{img:"17251.png", name:"Очки Prada"},
-{img:"17255.png", name:"Наушники"},
-{img:"17256.png", name:"Bearbrick"},
-{img:"17273.png", name:"Мини гелик"},
-
-{img:"175.png", name:"Конор"},
-{img:"17976.png", name:"Венок"},
-{img:"17996.png", name:"Ушанка LV корич"},
-{img:"17997.png", name:"Ушанка LV синяя"},
-
-{img:"18203.png", name:"Киса"},
-{img:"18895.png", name:"Лошадь"},
-
-{img:"19.png", name:"Баба"},
-{img:"194.png", name:"Хищник"},
-{img:"206.png", name:"Пацан"},
-
-{img:"269.png", name:"Жиробас"},
-{img:"270.png", name:"Шоколадка"},
-
-{img:"35.png", name:"Школьник серый"},
-{img:"36.png", name:"Школьник синий"},
-{img:"43.png", name:"Опер"},
-{img:"47.png", name:"Чёрный адик"},
-{img:"48.png", name:"Подтяжки"},
-{img:"5.png", name:"Рикардо"},
-{img:"51.png", name:"Качок"},
-{img:"79.png", name:"Off White"},
-
-{img:"8599.png", name:"Василич"},
-{img:"8793.png", name:"Броник Off White"},
-{img:"94.png", name:"Рубашка"},
-
-{img:"icon-gold-vip.png", name:"VIP Gold"},
-{img:"icon-platinum-vip.png", name:"VIP Platinum"},
-{img:"icon-silver-vip.png", name:"VIP Silver"}
-];
+.card img{
+width:140px;
+height:140px;
+object-fit:contain;
+}
